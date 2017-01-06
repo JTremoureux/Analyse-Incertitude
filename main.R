f61ba6c6391d6350b1fc87221f7ead478c11694d
Tremoureux Jordan <jordan.tremoureux@gmail.com>
2017-01-06 11:41:52 +0100

    wet

---
+++ main.R
@@ -0,0 +1,13 @@
+##################################
+## PROJET ANALYSE D'INCERTITUDE ##
+##################################
+
+
+library(DiceDesign) # Plan d’expérience, distance maximin, factDesign
+library(lhs) # Plan d’expérience LHS
+library(randtoolbox) # suite a discrépance faible (Cobol, Halton, ..)
+
+library(DiceKriging) # Processus Gaussiens
+
+library(evd)          # Loi Gumbel
+library(triangle)     # Loi triangulaire
