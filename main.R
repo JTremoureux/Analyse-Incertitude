0d6bd76302e91a8c892670c4c1de3aeaaa7a7c73
Thomas Wisniewski <gorrd34@gmail.com>
2017-01-06 14:53:38 +0100

    Update main.R

--- main.R
+++ main.R
@@ -2,18 +2,33 @@
 ## PROJET ANALYSE D'INCERTITUDE ##
 ##################################
 
+# Onn cherche à caractériser le comportement global de deux sorties représentatives de l’état final
+# du système (concentration en chlorite et concentration en calcite) en fonction des paramètres initiaux de 
+# l’expérience simulée.
+
 # Packages
-library(sensitivity)
+library(sensitivity) # Morris method
+library(corrplot) # Correlation
+library(randomForest)
+
+# MORRIS
 
 # Lecture des données
 jdd_1 <- read.csv("H:/GM/5GM/Analyse_incertitude/TP_final/projet_incertitude/input/jdd_1.csv", header=TRUE)
 jdd_1 <- as.data.frame(apply(apply(jdd_1, 2, gsub, patt=",", replace="."), 2, as.numeric))
+
+express_1 <- jdd_1[,-31:-30]
 
 # Variables
 taux_dissolution <- colnames(jdd_1)[startsWith(colnames(jdd_1), "K2diss")]
 taux_precipitation <- colnames(jdd_1)[startsWith(colnames(jdd_1), "Kppt")]
 surface_reaction <- colnames(jdd_1)[startsWith(colnames(jdd_1), "Kine")]
 sorties <- colnames(jdd_1)[startsWith(colnames(jdd_1), "X.wgt")]
+
+# Statistique descriptive
+
+mat.cor <- cor(jdd_1)
+corrplot(mat.cor, type="lower", method="color",tl.col="black",tl.srt=45)
 
 summary(jdd_1)
 
@@ -65,25 +80,43 @@ for(i in 1:2) {
 
 ## Modélisation par une regression linéaire
 
-full.lm1 <- lm(X.wgt_calcite ~ . - X.wgt_chlorite, data=jdd_1)
-full.lm2 <- lm(X.wgt_chlorite ~ . - X.wgt_calcite, data=jdd_1)
+full.lm1 <- lm(jdd_1$X.wgt_calcite ~ ., data=express_1)
+full.lm2 <- lm(jdd_1$X.wgt_chlorite ~ ., data=express_1)
 
-min.lm1 <- lm(X.wgt_calcite ~ 1 - X.wgt_chlorite, data=jdd_1)
+min.lm1 <- lm(jdd_1$X.wgt_calcite ~ 1, data=express_1)
 step.lm1 <- step(min.lm1, scope = list(lower=min.lm1,upper=full.lm1), direction = "both")
+
+selected.var <- all.vars(formula(step.lm1)[-2])
 
 ## Morris ça glisse
 
+binf = apply(express_1,2,min)
+bsup = apply(express_1,2,max)
 
 scale.unif <- function(X) {
-  binf = apply(X,1,min)
-  bsup = apply(X,1,max)
   return(X*(bsup-binf) + binf)
 }
 
 jdd_1.simule <- function(X) {
-  X.scaled <- data.frame(apply(X,1,scale.unif))
+  X.scaled <- data.frame(t(apply(X,1,scale.unif)))
   return(predict(full.lm1,X.scaled))
 }
 
-mor1 <- morris(model = jdd_1.simule, factors = colnames(jdd_1)[-31:-30], r = 6, design = list(type = "oat",levels = 5,grid.jump = 1))
+mor1 <- morris(model = jdd_1.simule, factors = colnames(express_1), r = 100, design = list(type = "oat",levels = 4, grid.jump = 3))
+plot(mor1)
+
+
+# ANOVA
+
+anova <- aov(jdd_1$X.wgt_calcite ~ (.)^2, data=express_1)
+plot(anova)
+
+
+# RANDOMFOREST
+
+rf_fit <- randomForest(jdd_1$X.wgt_calcite ~ ., data=express_1)
+varImpPlot(rf_fit)
+
+morf <- morris(model = rf_fit, factors = colnames(express_1), r = 10, design = list(type = "oat",levels = 4, grid.jump = 3))
+plot(morf)
 
